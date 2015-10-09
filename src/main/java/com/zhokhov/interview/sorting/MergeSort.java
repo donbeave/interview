e667843aa1b57ffb51af64d76ee5b334ea53f316
Alexey Zhokhov <donbeave@gmail.com>
2015-10-10 00:26:30 +0700

    Added highlight colors to merge sort.

--- src/main/java/com/zhokhov/interview/sorting/MergeSort.java
+++ src/main/java/com/zhokhov/interview/sorting/MergeSort.java
@@ -9,7 +9,7 @@
  */
 package com.zhokhov.interview.sorting;
 
-import static com.zhokhov.interview.util.Console.__dump;
+import static com.zhokhov.interview.util.Console.*;
 
 /**
  * @author <a href='mailto:alexey@zhokhov.com'>Alexey Zhokhov</a>
@@ -24,17 +24,22 @@ public class MergeSort {
      * The mergeSort algorithm implementation
      */
     private void mergeSort(int[] array, int left, int right) {
+        __grey("\nmergeSort, left: " + left + ", right: " + right + "  ==> ");
+        ____purple("" + (left < right));
 
         if (left < right) {
-
-            //split the array into 2
+            // split the array into 2
             int center = (left + right) / 2;
 
-            //sort the left and right array
+            ____grey("center: " + center);
+
+            // sort the left and right array
             mergeSort(array, left, center);
             mergeSort(array, center + 1, right);
 
-            //merge the result
+            ____grey("Merging, center: " + center);
+
+            // merge the result
             merge(array, left, center + 1, right);
         }
     }
@@ -44,12 +49,16 @@ public class MergeSort {
      */
     private void merge(int[] array, int leftArrayBegin,
                        int rightArrayBegin, int rightArrayEnd) {
-
         int leftArrayEnd = rightArrayBegin - 1;
 
         int numElements = rightArrayEnd - leftArrayBegin + 1;
         int[] resultArray = new int[numElements];
         int resultArrayBegin = 0;
+
+        ____grey("  leftArrayBegin: " + leftArrayBegin);
+        ____grey("  leftArrayEnd: " + leftArrayEnd);
+        ____grey("  rightArrayBegin: " + rightArrayBegin);
+        ____grey("  rightArrayEnd: " + rightArrayEnd);
 
         // Find the smallest element in both these array and add it to the result
         // array i.e you may have a array of the form [1,5] [2,4]
@@ -57,8 +66,12 @@ public class MergeSort {
         while (leftArrayBegin <= leftArrayEnd && rightArrayBegin <= rightArrayEnd) {
             if (array[leftArrayBegin] <= array[rightArrayBegin]) {
                 resultArray[resultArrayBegin++] = array[leftArrayBegin++];
+
+                ____blue(" new leftArrayBegin: " + leftArrayBegin);
             } else {
                 resultArray[resultArrayBegin++] = array[rightArrayBegin++];
+
+                ____blue(" new rightArrayBegin: " + rightArrayBegin);
             }
         }
 
@@ -66,12 +79,16 @@ public class MergeSort {
         // left array copy them first
         while (leftArrayBegin <= leftArrayEnd) {
             resultArray[resultArrayBegin++] = array[leftArrayBegin++];
+
+            ____blue(" [post] new leftArrayBegin: " + leftArrayBegin);
         }
 
         // After the main loop completed we may have few more elements in
         // right array copy them
         while (rightArrayBegin <= rightArrayEnd) {
             resultArray[resultArrayBegin++] = array[rightArrayBegin++];
+
+            ____blue(" [post] new rightArrayBegin: " + rightArrayBegin);
         }
 
         // Copy resultArray back to the main array
@@ -87,7 +104,6 @@ public class MergeSort {
      * This is here only for explanation purpose
      */
     public int[] sampleMerge(int[] leftArray, int[] rightArray) {
-
         int leftArrayEnd = leftArray.length - 1;
         int rightArrayEnd = rightArray.length - 1;
         int leftArrayBegin = 0;
@@ -97,14 +113,23 @@ public class MergeSort {
         int[] resultArray = new int[numElements];
         int resultArrayBegin = 0;
 
+        ____grey("  leftArrayBegin: " + leftArrayBegin);
+        ____grey("  leftArrayEnd: " + leftArrayEnd);
+        ____grey("  rightArrayBegin: " + rightArrayBegin);
+        ____grey("  rightArrayEnd: " + rightArrayEnd);
+
         // Find the smallest element in both these array and add it to the temp
         // array i.e you may have a array of the form [1,5] [2,4]
         // We need to sort the above as [1,2,4,5]
         while (leftArrayBegin <= leftArrayEnd && rightArrayBegin <= rightArrayEnd) {
             if (leftArray[leftArrayBegin] <= rightArray[rightArrayBegin]) {
                 resultArray[resultArrayBegin++] = leftArray[leftArrayBegin++];
+
+                ____blue(" new leftArrayBegin: " + leftArrayBegin);
             } else {
                 resultArray[resultArrayBegin++] = rightArray[rightArrayBegin++];
+
+                ____blue(" new rightArrayBegin: " + rightArrayBegin);
             }
         }
 
@@ -112,36 +137,52 @@ public class MergeSort {
         // left array copy them first
         while (leftArrayBegin <= leftArrayEnd) {
             resultArray[resultArrayBegin++] = leftArray[leftArrayBegin++];
+
+            ____blue(" [post] new leftArrayBegin: " + leftArrayBegin);
         }
 
         // After the main loop completed we may have few more elements in
         // right array copy them
         while (rightArrayBegin <= rightArrayEnd) {
             resultArray[resultArrayBegin++] = rightArray[rightArrayBegin++];
+
+            ____blue(" [post] new rightArrayBegin: " + rightArrayBegin);
         }
 
         return resultArray;
     }
 
     public static void main(String args[]) {
-
-        System.out.println("Running mergeSort....");
-        System.out.println("Running merge sort on..{7, 1, 8, 2, 0, 12, 10, 7, 5, 3}..");
         int array[] = {7, 1, 8, 2, 0, 12, 10, 7, 5, 3};
 
-        MergeSort mergeSort = new MergeSort();
-
-        mergeSort.sort(array);
-
+        __yellow("\nNew array: ");
         __dump(array);
 
-        System.out.println("Now demo a simple merge routine....");
-        System.out.println("Merging..{1, 3, 5, 7} and ..{2, 4, 6, 8, 10}..");
+        System.out.println("\nSorting");
+
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(array);
+
+        __green("\nResult: ");
+        __dump(array);
+
+        System.out.print("\n");
+        System.out.println("***********************\n");
+        System.out.println("Now demo a simple merge routine.");
+
         int leftArray[] = {1, 3, 5, 7};
         int rightArray[] = {2, 4, 6, 8, 10};
 
+        __yellow("\nLeft array: ");
+        __dump(leftArray);
+        __yellow("Right array: ");
+        __dump(rightArray);
+
+        System.out.println("\nMerging");
+
         int[] mergedArray = mergeSort.sampleMerge(leftArray, rightArray);
 
+        __green("\nResult: ");
         __dump(mergedArray);
     }
 
